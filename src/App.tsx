209d96110046ba41e00f2e33035cc20365c02ad3
water-plant <arsconduit@gmail.com>
2024-02-06 16:12:55 -0500

    task 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>Name: Edwin</p>
+            <p>&quot;Hello World&quot;</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
