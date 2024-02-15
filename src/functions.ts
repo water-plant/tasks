27b68043f0315ddc830845065a2e3ce0133578fb
water-plant <arsconduit@gmail.com>
2024-02-14 19:31:56 -0500

    functions complete

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,7 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return ((temperature - 32) * 5) / 9;
 }
 
 /**
@@ -12,15 +12,25 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
-}
+    let sum = 0;
 
+    if (first > 0) {
+        sum += first;
+    }
+    if (second > 0) {
+        sum += second;
+    }
+    if (third > 0) {
+        sum += third;
+    }
+    return sum;
+}
 /**
  * Consumes a string and produces the same string in UPPERCASE and with an exclamation
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return `${message.toUpperCase()}!`;
 }
 
 /**
@@ -28,7 +38,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.charAt(message.length - 1) == "?" ? true : false;
 }
 
 /**
@@ -37,5 +47,11 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word.toLowerCase() == "yes") {
+        return true;
+    }
+    if (word.toLowerCase() == "no") {
+        return false;
+    }
+    return null;
 }
