f37003bcd739658ba5ed2cbba48537fb118ecb74
water-plant <arsconduit@gmail.com>
2024-02-21 17:59:33 -0500

    duplicateQuestion finished

--- src/objects.ts
+++ src/objects.ts
@@ -83,7 +83,8 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    const newQuestion: Question = { ...question, name: newName };
+    return newQuestion;
 }
 
 /**
@@ -92,7 +93,11 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    const newQuestion: Question = {
+        ...question,
+        published: !question.published
+    };
+    return newQuestion;
 }
 
 /**
