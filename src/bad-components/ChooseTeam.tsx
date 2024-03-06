54c6c574c03c98fd31ae535575bc9c67ff9b7732
water-plant <arsconduit@gmail.com>
2024-03-05 23:40:48 -0500

    double half and chooseteams are done

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -14,18 +14,16 @@ export function ChooseTeam(): JSX.Element {
     const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
-        /*
+    function chooseMember(newMember: string) {
         if (!team.includes(newMember)) {
             team.push(newMember);
         }
-        */
+        setTeam([...team]);
     }
 
     function clearTeam() {
-        /*
-        team = [];
-        */
+        team.splice(0, team.length);
+        setTeam([]);
     }
 
     return (
@@ -36,7 +34,10 @@ export function ChooseTeam(): JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
