28f9270a735c52322b3c5232779e398224cd9867
water-plant <arsconduit@gmail.com>
2024-03-07 18:51:34 -0500

    CheckAnswer EditMode GiveAttempt

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { bookEndList } from "../arrays";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
@@ -7,9 +8,21 @@ export function CheckAnswer({
     expectedAnswer: string;
 }): JSX.Element {
     const [answer, setAnswer] = useState<string>("");
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+    }
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="shortResponseInput">
+                <Form.Control
+                    value={answer}
+                    onChange={updateAnswer}
+                    type="answer"
+                    placeholder="short response"
+                />
+            </Form.Group>
+            {expectedAnswer === answer ? "✔️" : "❌"}
         </div>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,47 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [name, setName] = useState<string>("Your Name");
+    const [student, setStudent] = useState<boolean>(true);
+    const [a, setA] = useState<boolean>(false);
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                inline
+                type="switch"
+                id="edit"
+                label="edit"
+                checked={a}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setA(e.target.checked)
+                }
+            />
+            <Form.Group>
+                <Form.Control
+                    disabled={!a}
+                    value={name}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setName(e.target.value)
+                    }
+                />
+            </Form.Group>
+            <Form.Check
+                inline
+                type="checkbox"
+                id="edit"
+                label="student?"
+                disabled={!a}
+                checked={student}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setStudent(e.target.checked)
+                }
+            />
+            <span>
+                {" "}
+                {name} is {student ? "a" : "not a"} student.
+            </span>
         </div>
     );
 }
